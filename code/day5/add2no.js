5bb8d88e46fc582b2c726305ba3d6464fade3daa
prateek17shu <40847075+prateek17shu@users.noreply.github.com>
2018-08-06 11:23:06 +0530

    Create add2no.js

---
+++ code/day5/add2no.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2> Adding two no in Java-Script</h2>
+<p id="demo"> </p>
+<script>
+var x=10;
+var y=20;
+var z= x + y;
+document.getElementById("demo").innerHTML =z;
+</script>
+</body>
+</html>
+
